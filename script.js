1303f4706a0998974294304ccdce49c67590d062
harry-fruit <63006188+harry-fruit@users.noreply.github.com>
2021-03-01 09:22:48 -0300

    separando arquivos

---
+++ script.js
@@ -0,0 +1,31 @@
+ //DeleteData
+ function apagarInfo (){
+    document.getElementById('printer').innerHTML = ''
+}
+
+// Add Value
+function addValue(data){
+    const newValor = data
+    const currentValue = document.getElementsByTagName('p')[0].innerText
+
+    if(currentValue.length == 50){
+        document.getElementById('printer').innerHTML = currentValue
+    }else{
+        document.getElementById('printer').innerHTML = currentValue.concat(newValor)
+    }
+}
+// = 
+function calcularResultado(){
+    const resultado = document.getElementById('printer').innerText
+        document.getElementById('printer').innerHTML = eval(resultado)
+}
+
+//factorial
+function factorial (numero){
+    let currentValue = 1
+    for(let i = 1; i <= numero; i++){
+        currentValue *= i
+    }
+    document.querySelector('#printer').innerHTML = currentValue
+}
+
