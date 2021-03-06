fb318fd7d61e053be3fe75c3640d842d3e8ff39b
harry-fruit <63006188+harry-fruit@users.noreply.github.com>
2021-03-06 19:30:50 -0300

    styleTotallyFixed

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
- //DeleteData
- function apagarInfo (){
-    document.getElementById('printer').innerHTML = ''
-}
-
-function addValue(data){
-    const newValor = data
-    const currentValue = document.getElementsByTagName('p')[0].innerText
-
-    if(currentValue.length == 50){
-        document.getElementById('printer').innerHTML = currentValue
-    }else{
-        document.getElementById('printer').innerHTML = currentValue.concat(newValor)
+const armazemDeFuncoes = {
+    apagarInfo (){
+        document.getElementById('printer').innerHTML = ''
+    },
+    addValue(data){
+        const newValor = data
+        const currentValue = document.getElementsByTagName('p')[0].innerText
+    
+        if(currentValue.length == 50){
+            document.getElementById('printer').innerHTML = currentValue
+        }else{
+            document.getElementById('printer').innerHTML = currentValue.concat(newValor)
+        }
+    },
+    calcularResultado(){
+        const resultado = document.getElementById('printer').innerText
+            document.getElementById('printer').innerHTML = eval(resultado)
+    },
+    factorial (numero){
+        if(numero){
+            let currentValue = 1
+        for(let i = 1; i <= numero; i++){
+            currentValue *= i
+        }
+        document.querySelector('#printer').innerHTML = currentValue
+        }else{
+            alert('Primeiramente insira um valor')       
+        }
+    },
+    clicaveis (){
+        const but = document.getElementsByClassName('btt')
+        
+        for(let i = 0; i < but.length; i++){
+            document.getElementsByClassName('btt')[i].addEventListener('click', e=>{
+                const currentValue = document.querySelector('#printer').innerText
+                const newValue = but[i].innerText
+                
+                if(currentValue.length == 50){
+                    document.querySelector('#printer') = currentValue
+                }else{
+                    document.querySelector('#printer').innerHTML = currentValue.concat(newValue)
+                }
+            })
+        }
     }
 }
 
-// = 
-function calcularResultado(){
-    const resultado = document.getElementById('printer').innerText
-        document.getElementById('printer').innerHTML = eval(resultado)
-}
+const delInfo = armazemDeFuncoes.apagarInfo
+const addValor = armazemDeFuncoes.addValue
+const calcResultado = armazemDeFuncoes.calcularResultado
+const fatorial = armazemDeFuncoes.factorial
+const clickButton = armazemDeFuncoes.clicaveis
 
-//factorial
-function factorial (numero){
-    if(numero){
-        let currentValue = 1
-    for(let i = 1; i <= numero; i++){
-        currentValue *= i
-    }
-    document.querySelector('#printer').innerHTML = currentValue
-    }else{
-        alert('Primeiramente insira um valor')       
-    }
-}
 
